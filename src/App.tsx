22d2c2b2fd96253483a585614ea5e79ed3152b58
Sepehr sanaee <sepehr_sanaee78@yahoo.com>
2021-06-26 13:21:47 +0430

    post old components to new structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {currentHeight} from "./ergo-related/explorer";
+// import {currentHeight} from "./ergo-related/explorer";
 import MyErgoPage from "./my-ergo-page";
 
 function App() {
---
+++ src/App/routes/Team/index.js.js
@@ -0,0 +1,5 @@
+import React from './node_modules/react'
+
+export default function Team(){
+
+}
